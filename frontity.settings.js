66ce2347353e669fc9472d10f2769e3e49ea0171
Michael VERGOZ <mykiimike@users.noreply.github.com>
2020-06-26 23:06:47 +0200

    Initial commit

---
+++ frontity.settings.js
@@ -0,0 +1,41 @@
+const settings = {
+  name: "codesandbox-frontity",
+  state: {
+    frontity: {
+      url: "https://test.frontity.io",
+      title: "Pulse.digital"
+    }
+  },
+  packages: [
+    {
+      name: "@frontity/mars-theme",
+      state: {
+        theme: {
+          menu: [
+            ["Home", "/"],
+            ["Nature", "/category/nature/"],
+            ["Travel", "/category/travel/"],
+            ["Japan", "/tag/japan/"],
+            ["About Us", "/about-us/"]
+          ],
+          featured: {
+            showOnList: false,
+            showOnPost: false
+          }
+        }
+      }
+    },
+    {
+      name: "@frontity/wp-source",
+      state: {
+        source: {
+          api: "https://test.frontity.io/wp-json"
+        }
+      }
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react"
+  ]
+};
+
+export default settings;
